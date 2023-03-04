b61e1e36d73f8a5403864bb3a3dd047885d471d4
christopher lopez <christopherwlopez89@gmail.com>
2023-03-04 14:48:50 -0800

    reactored form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-
 import './Form.scss';
+import { useState } from 'react';
 
-const Form = (props) => {
+const Form = ({handleApiCall}) => {
 
+  const [ method, setMethod ] = useState("");
+  const [ url, setUrl ] = useState("");
+  const [ text, changeText] = useState("");
+  
   const handleSubmit = e => {
     e.preventDefault();
     const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
-    };
-    this.props.handleApiCall(formData);
+      method:method,
+      url: url,
+      text: text,
+    }
+    handleApiCall(formData);
+    console.log(formData);
   }
 
 
@@ -19,16 +25,27 @@ const Form = (props) => {
         <form onSubmit={handleSubmit}>
           <label >
             <span>URL: </span>
-            <input name='url' type='text' />
+            <input  type='text' value={url} onChange={(e)=>setUrl(e.target.value)}/>
             <button type="submit">GO!</button>
           </label>
+
           <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
+            <input type="radio" value="GET" name="method" onChange={(e)=> setMethod(e.target.value)} />GET
+          </label>
+          <label className="methods">
+            <input type="radio" value="POST" name="method" onChange={(e)=> setMethod(e.target.value)} />POST
+          </label>
+          <label className="methods">
+            <input type="radio" value="PUT" name="method" onChange={(e)=> setMethod(e.target.value)} />PUT
+          </label>
+          <label className="methods">
+            <input type="radio" value="DELETE" name="method" onChange={(e)=> setMethod(e.target.value)} />DELETE
+          </label>
+          <label> 
+            <textarea value={text} onChange={(e)=> changeText(e.target.value)}></textarea>
           </label>
         </form>
+
       </>
     );
   };
